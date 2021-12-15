5eb4124c8d792a28e5a4694c8a909db767ae89e8
Armagan Anteplioglu <armagananteplioglu@gmail.com>
2021-12-15 06:09:31 +0200

    day2

--- script.js
+++ script.js
@@ -7,17 +7,30 @@ const operatorButtons = document.querySelectorAll(".operator")
 const dot = document.querySelector(".dot")
 const equals = document.querySelector(".equals")
 const display = document.querySelector(".display")
-let firstValue = 0;
+let newValue;
+let oldValue;
+let firstValue;
 let secondValue;
 
 //functions
 
-function displayNumber(number) {
-    display.innerHTML = firstValue
+function displayNumber(newValue) {
+    if (oldValue == undefined) {
+        display.textContent = parseInt(newValue);
+        oldValue = newValue
+    }
+
+    else {
+        display.textContent = oldValue + newValue
+        oldValue = oldValue + newValue
+    }
 }
 
 //button event listeners
 
 numberButtons.forEach (number => {
-    number.addEventListener ("click", console.log (number.innerText))
+    number.addEventListener ("click", () =>{
+        newValue = number.innerHTML
+        displayNumber(newValue)
+    })
 })
