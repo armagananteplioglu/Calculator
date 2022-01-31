b3d1bbb268fb2fd954341f32026df866279754c5
ultimatenoob99 <71595820+ultimatenoob99@users.noreply.github.com>
2022-01-31 13:34:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,208 @@
-const numberButtons = document.querySelectorAll(".number")
-const allClear = document.querySelector (".all-clear")
-const del = document.querySelector(".del")
+// consts
+
+const numbers = document.querySelectorAll (".number")
+const ac = document.querySelector ("#ac")
+const del = document.querySelector ("#del")
+const sqrt = document.querySelector ("#sqrt")
+const factorial = document.querySelector ("#factorial")
 const operatorButtons = document.querySelectorAll(".operator")
-const dot = document.querySelector(".dot")
-const equals = document.querySelector(".equals")
-const display = document.querySelector(".display")
-const allButtons = document.querySelectorAll("button")
+const equals = document.querySelector ("#equals")
+const display = document.querySelector (".display")
+const minordisplay = document.querySelector(".minordisplay")
+const dot = document.querySelector("#dot")
+
+let firstOperand = ""
+let secondOperand = ""
+let currentOperatorSymbol = ""
+let previousOperatorSymbol = ""
+let result = ""
+let shouldResetScreen = false
+
 
 //functions
 
-// function clearAll () {
-//     display.innerText = "";
-// }
-
-
-
-function equalButton() {
-    display.innerText += eval(display)
+function appendNumber(number) {
+  if (display.textContent === '0' || shouldResetScreen)
+    resetScreen()
+  display.textContent += number
 }
-//event listeners 
 
-// numberButtons.forEach (button => {
-//     button.addEventListener("click", (e) => {
-//         display.innerText += e.target.innerText
-//     })
-// })
+function resetScreen() {
+  display.textContent = ''
+  shouldResetScreen = false
+}
 
-allButtons.forEach (a => {
-    a.addEventListener ("click", (e) => {
-        switch (e.target.innerText){
-            case "AC":
-                display.innerText = "";
-                break;
-            case "DEL":
-                if (display.innerText) {
-                    display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            case "=":
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Invalid Expression"
-                }
-                
-                break;
-            default:
-                display.innerText += e.target.innerText
-        }
-        
-    })
-})
+function clearEverything () {
+  display.textContent = "";
+  minordisplay.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  result = "";
+  currentOperatorSymbol = "";
+  previousOperatorSymbol = "";
+}
 
-allClear.addEventListener ("click", clearAll)
+function deleteLast () {
+  display.textContent = Number(display.textContent.toString().slice(0, -1));
+  if (minordisplay.textContent === 0) {
+    display.textContent = ""
+  }
+}
 
-del.addEventListener ("click", delLast)
+function setOperator (operator) {
+  if (firstOperand == "" || display.textContent == "") {
+    currentOperatorSymbol = operator
+    updateDisplay()
+  }
+  else if (typeof result == "number") {
+    previousOperatorSymbol = currentOperatorSymbol
+    currentOperatorSymbol = operator
+    updateDisplay()
+  }
+  else {
+    previousOperatorSymbol = currentOperatorSymbol
+    currentOperatorSymbol = operator
+    compute();
+  }
+}
 
-equals.addEventListener("click", equalButton)
+function updateDisplay() {
+  if (firstOperand == "") {
+    firstOperand = Number(display.textContent)
+    minordisplay.textContent = `${firstOperand} ${currentOperatorSymbol}`
+    display.textContent = ""
+  }
+
+  else if (display.textContent == "") {
+    minordisplay.textContent = `${firstOperand} ${currentOperatorSymbol}`
+  }
+
+  else if (typeof result == "number") {
+    firstOperand = result
+    minordisplay.textContent = `${result} ${currentOperatorSymbol}`
+    result = ""
+    display.textContent = ""
+  }
+}
+
+function equalsButton () {
+  previousOperatorSymbol = currentOperatorSymbol
+  if (display.textContent == "") {
+    return
+  }
+  secondOperand = Number(display.textContent)
+  switch (previousOperatorSymbol) {
+    case "+":
+      add(firstOperand, secondOperand)
+      break;
+    case "-":
+      substract(firstOperand, secondOperand)
+      break;
+    case "*":
+      multiply(firstOperand, secondOperand)
+      break;
+    case "/":
+      divide(firstOperand, secondOperand)
+      break;
+  }
+  minordisplay.textContent = `${firstOperand} ${previousOperatorSymbol} ${secondOperand}`
+  display.textContent = result
+  secondOperand = ""
+  firstOperand = result
+}
+
+function compute () {
+  if (display.textContent == "") {
+    return
+  }
+  secondOperand = Number(display.textContent)
+  switch (previousOperatorSymbol) {
+    case "+":
+      add(firstOperand, secondOperand)
+      updateDisplay()
+      secondOperand = ""
+      break;
+    case "-":
+      substract(firstOperand, secondOperand)
+      updateDisplay()
+      secondOperand = ""
+      break;
+    case "*":
+      multiply(firstOperand, secondOperand)
+      updateDisplay()
+      secondOperand = ""
+      break;
+    case "/":
+      divide(firstOperand, secondOperand)
+      updateDisplay()
+      secondOperand = ""
+      break;
+  }
+}
+
+
+function add(a, b) {
+  result = a + b
+}
+
+function substract(a, b) {
+  result = a - b
+}
+
+function multiply(a, b) {
+  result = a * b
+}
+
+function divide(a, b) {
+  if (b == 0) {
+    display.textContent = "Undefined"
+    return
+  }
+  result = a / b
+}
+
+function squareRoot () {
+  minordisplay.textContent = "√" + display.textContent
+  result = Math.sqrt(Number(display.textContent))
+  display.textContent = result;
+}
+
+function calcFactorial () {
+  minordisplay.textContent = display.textContent + "!"
+  let a = Math.floor(Number(display.textContent));
+  if (a === 0 || a === 1) {
+    display.textContent = 1;
+  }
+  else {
+    for (let i = a - 1; i>= 1; i--) {
+      a *= i
+      result = a
+      }
+    display.textContent = result;
+
+  }
+}
+
+function addDot () {
+  if (display.textContent.toString().includes(".")) {
+    return
+  }
+  else {
+    display.textContent += "."
+  }
+}
+
+//event listeners
+
+numbers.forEach((number) => 
+  number.addEventListener("click", () => appendNumber(number.textContent))
+)
+operatorButtons.forEach((operatorButton) => 
+  operatorButton.addEventListener("click", () => setOperator(operatorButton.textContent))
+)
+ac.addEventListener ("click", clearEverything)
+del.addEventListener("click", deleteLast)
+equals.addEventListener("click", equalsButton)
+sqrt.addEventListener("click", squareRoot)
+factorial.addEventListener("click", calcFactorial)
+dot.addEventListener("click", addDot)
